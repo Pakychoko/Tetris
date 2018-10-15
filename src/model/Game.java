1098422fee8b6e7ab3386ec34231f7bb2dbf9847
Fran Azorin <fazoring@everis.com>
2018-10-15 13:24:34 +0200

    v0.2.4 Doc and Comments Added

--- src/model/Game.java
+++ src/model/Game.java
@@ -12,15 +12,29 @@ package model;
  */
 public class Game {
 
+	/**
+	 * Atributo booleano que indica si el juego ha acabado
+	 */
 	private boolean gameEnded;
-	
+	/**
+	 * Atributo Gameboard que almacena un tablero
+	 */
 	private Gameboard board;
-	
+	/**
+	 * Atributo Piece que almacena la pieza actual
+	 */
 	private Piece currentPiece;
-	
+	/**
+	 * Atributo Coordinate que almacena la posicion actual
+	 */
 	private Coordinate currentPosition;
 	
 	
+	/**
+	 * Constructor de la clase Game
+	 * Construye un nuevo tablero y pone el juego como NO finalizado
+	 * @param c Coordenada tamaño del tablero para iniciarlo
+	 */
 	public Game(Coordinate c) {
 		
 		this.board = new Gameboard(c);
@@ -29,6 +43,9 @@ public class Game {
 	
 	
 	//MOVEMENT METHODS
+	/**
+	 * Mueve la pieza a la izquierda
+	 */
 	public void moveCurrentPieceLeft() {
 		
 		Coordinate newC = currentPosition.add(new Coordinate(0, -1));
@@ -43,6 +60,9 @@ public class Game {
 		}
 	}
 	
+	/**
+	 * Mueve la pieza a la derecha
+	 */
 	public void moveCurrentPieceRight() {
 		
 		Coordinate newC = currentPosition.add(new Coordinate(0, 1));
@@ -56,6 +76,9 @@ public class Game {
 		}
 	}
 	
+	/**
+	 * Mueve la pieza hacia abajo
+	 */
 	public void moveCurrentPieceDown() {
 		
 		this.currentPosition = currentPosition.add(new Coordinate(1, 0));
@@ -71,6 +94,9 @@ public class Game {
 		}
 	}
 	
+	/**
+	 * Rota la pieza en sentido antihorario
+	 */
 	public void rotateCurrentPieceCounterclockwise() {
 		
 		this.currentPiece.rotateCounterclockwise();
@@ -78,6 +104,9 @@ public class Game {
 		this.board.putPiece(currentPosition, currentPiece);
 	}
 	
+	/**
+	 * Rota la pieza en sentido horario
+	 */
 	public void rotateCurrentPieceClockwise() {
 		
 		this.currentPiece.rotateClockwise();
@@ -87,6 +116,10 @@ public class Game {
 	
 	
 	//GAME METHODS
+	/**
+	 * Crea una nueva pieza y la coloca en lo alto del tablero.
+	 * @return Boolean que indica si hay pieza nueva
+	 */
 	public boolean nextPiece() {
 		
 		this.currentPosition = new Coordinate(0, board.getWidth() / 2 - 2);
@@ -103,15 +136,27 @@ public class Game {
 		return next;
 	}
 	
+	/**
+	 * Comprueba si la pieza esta fija
+	 * @return Boolean que indica si la pieza esta fija (True) o si NO lo esta (False)
+	 */
 	public boolean isCurrentPieceFixed() {
 		return this.currentPiece.isFixed();
 	}
 	
+	/**
+	 * Comprueba si el juego ha acabado
+	 * @return Boolean que indica si ha acabado (True) o si NO ha acabado (False)
+	 */
 	public boolean isGameEnded() {	
 		return this.gameEnded;
 	}
 	
 	//OTHER METHODS
+	/**
+	 * Devuelve un string con las posiciones del tablero
+	 * @return String con las posiciones del tablero
+	 */
 	@Override
 	public String toString() {
 		return this.board.toString();
--- src/model/Gameboard.java
+++ src/model/Gameboard.java
@@ -14,13 +14,27 @@ import java.util.Set;
  */
 public class Gameboard {
 
+	/**
+	 * Atributo de clase Gameboard que contiene un Mapa con las piezas y sus posiciones
+	 */
 	private Map<Coordinate, Piece> gameboard = new HashMap<Coordinate, Piece>();
 	
+	/**
+	 * Atributo de clase Gameboard que indica la altura del tablero
+	 */
 	private int height;
 	
+	/**
+	 * Atributo de clase Gameboard que indica la anchura del tablero
+	 */
 	private int width;
 	
 	
+	/**
+	 * Constructor de clase Gameboard
+	 * Construye un tablero a partir de la fila y la columna de la coordenada
+	 * @param c Coordenada que contiene el tamaño del tablero
+	 */
 	public Gameboard(Coordinate c) {
 		this.height = c.getRow();
 		this.width = c.getColumn();
@@ -28,23 +42,37 @@ public class Gameboard {
 	}
 	
 	
+	/**
+	 * Metodo que obtiene la anchura del tablero
+	 * @return width Anchura del tablero
+	 */
 	public int getWidth() {
 		return this.width;
 	}
 	
+	/**
+	 * Metodo que obtiene la altura del tablero
+	 * @return height Altura del tablero
+	 */
 	public int getHeight() {
 		return this.height;
 	}
 	
 	
+	/**
+	 * Metodo que comprueba si la posicion es valida o no
+	 * @param c Coordinate
+	 * @param p Piece
+	 * @return Boolean que indica si la posicion es valida (True) o si NO lo es (False)
+	 */
 	public boolean isPlaceValid(Coordinate c, Piece p) {
 
 		boolean valid = true;
-		Set<Coordinate>squares= p.getAbsoluteCells(c);
+		Set<Coordinate>squares = p.getAbsoluteCells(c);
 		
 		for (Coordinate cor : squares) {
 			if (cor.getRow() > this.getHeight() - 1 || cor.getColumn() > this.getWidth() - 1
-					|| cor.getColumn() < 0 || cor.getRow() < 0) {// si es mayor o menor nos devolverá false
+					|| cor.getColumn() < 0 || cor.getRow() < 0) {
 				valid = false;
 			}
 		}
@@ -52,6 +80,13 @@ public class Gameboard {
 		return valid;
 	}
 	
+	/**
+	 * Metodo que comprueba si la posicion esta libre o no
+	 * 
+	 * @param c Coordinate
+	 * @param p Piece
+	 * @return Boolean que indica si la posicion esta libre (True) o si NO lo esta (False)
+	 */
 	public boolean isPlaceFree(Coordinate c, Piece p) {
 		
 		boolean free = true;
@@ -67,6 +102,11 @@ public class Gameboard {
 	}
 
 	
+	/**
+	 * Metodo que pone una pieza en el tablero
+	 * @param c Coordinate
+	 * @param p Piece
+	 */
 	public void putPiece(Coordinate c, Piece p) {
 
 		Set<Coordinate> squares = p.getAbsoluteCells(c);
@@ -75,6 +115,10 @@ public class Gameboard {
 		}
 	}
 	
+	/**
+	 * Metodo que elimina una pieza del tablero
+	 * @param p Piece
+	 */
 	public void removePiece(Piece p) {
 	
 		for (int i = 0; i < width; i++) {
@@ -94,6 +138,12 @@ public class Gameboard {
 		}
 	}
 	
+	/**
+	 * Devuelve una referencia a la pieza contenida en la posición indicada 
+	 * por el parámetro o null si esta posicion está vacía
+	 * @param c Coordinate
+	 * @return Piece (Referencia)
+	 */
 	public Piece getCellContent(Coordinate c) {
 		
 		Piece p;
@@ -101,11 +151,19 @@ public class Gameboard {
 		return p;
 	}
 	
+	/**
+	 * Añade la referencia pasada como segundo parámetro 
+	 * al tablero en la posición indicada por el primer parámetro.
+	 * @param c Coordinate
+	 * @param p Piece
+	 */
 	public void setCellContent(Coordinate c, Piece p) {
 		gameboard.put(c, p);
 	}
 	
-	
+	/**
+	 * Metodo que devuelve un String con los simbolos del tablero y las piezas
+	 */
 	public String toString() {
 		
 		String board = new String();
